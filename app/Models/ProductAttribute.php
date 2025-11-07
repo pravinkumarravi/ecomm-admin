d3d705684895a3b1e7b53e0ef066d04cfe8c4b14
PBICF <personnelbranch.icf@gmail.com>
2025-11-07 16:39:14 +0530

    initial commit

--- app/Models/ProductAttribute.php
+++ app/Models/ProductAttribute.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Attribute extends Model
+class ProductAttribute extends Model
 {
     protected $fillable = [
         'product_id',
